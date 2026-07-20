c0dd8a04a00fb33fa35e75f4aa69a718248a92da
Farhan Shahriyar <f.shahriyar@a1qa.com>
2026-07-20 22:15:22 +0600

    changed browser setup

--- playwright.config.js
+++ playwright.config.js
@@ -4,14 +4,22 @@ const isCI = Boolean(process.env.CI);
 
 export default defineConfig({
   testDir: './tests',
+
   timeout: 30000,
+
   expect: {
     timeout: 6000,
   },
+
   retries: isCI ? 2 : 0,
+
   reporter: isCI
-    ? [['list'], ['html', { open: 'never' }]]
+    ? [
+        ['list'],
+        ['html', { open: 'never' }],
+      ]
     : 'list',
+
   use: {
     baseURL: 'https://userinyerface.com',
     headless: true,
@@ -19,11 +27,24 @@ export default defineConfig({
     video: 'retain-on-failure',
     trace: 'retain-on-failure',
   },
+
   projects: [
     {
       name: 'chromium',
       use: {
         ...devices['Desktop Chrome'],
+      },
+    },
+    {
+      name: 'firefox',
+      use: {
+        ...devices['Desktop Firefox'],
+      },
+    },
+    {
+      name: 'webkit',
+      use: {
+        ...devices['Desktop Safari'],
       },
     },
   ],
